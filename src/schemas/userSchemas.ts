572b2a19b9304564f0c6b1378006d16eeb773e11
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-07-28 16:16:17 -0400

    feat: create user schema

---
+++ src/schemas/userSchemas.ts
@@ -0,0 +1,10 @@
+import { UserRole } from '@prisma/client';
+import Joi from 'joi';
+import CreateUserType from "../@types/types";
+
+export const createUserSchema = Joi.object<CreateUserType>({
+  name: Joi.string().min(1).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().valid(...Object.values(UserRole)).required(),
+});
