275803fd54ad97e360457ac278550f2362cebb43
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-07-28 08:34:50 -0400

    fix: generate Prisma client and update configuration

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,9 @@
-console.log("hellow world")
+import app, { init } from './app';
+
+const port = 5000;
+
+init().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
+  });
+});
